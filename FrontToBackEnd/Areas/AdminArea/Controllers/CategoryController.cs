35d2db690f230018a7f886a8aa4ae6896adf2875
Rashad Gasimzada <helloqwerty00@icloud.com>
2022-03-27 23:12:48 +0400

    homework added

---
+++ FrontToBackEnd/Areas/AdminArea/Controllers/CategoryController.cs
@@ -0,0 +1,61 @@
+﻿using FrontToBackEnd.Data;
+using FrontToBackEnd.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FrontToBackEnd.Areas.Areas.Controllers
+{
+    [Area("AdminArea")]
+    public class CategoryController : Controller
+    {
+        
+        private readonly AppDbContext _context;
+            public CategoryController(AppDbContext context)
+            {
+                _context = context;
+            }
+        public async Task<IActionResult> Index()
+        {
+
+            List<Category> categories = await _context.Categories.Where(m => m.IsDeleted == false).ToListAsync();
+            return View(categories);
+        }
+
+        public IActionResult Detail(int id)
+            {
+                var category = _context.Categories.FirstOrDefault(m => m.Id == id);
+                return View(category);
+                //return Json(new
+                //{
+                //    categoryName = category.Name,
+                //    cetegoryDet=category.Products,
+                //    action = "Detail",
+                //    Id = id
+
+                //}); 
+            }
+            public IActionResult Edit(int id)
+            {
+                return Json(new
+                {
+                    action = "Edit",
+                    Id = id
+
+                });
+            }
+            public IActionResult Delete(int id)
+            {
+                return Json(new
+                {
+                    action = "Delete",
+                    Id = id
+
+                });
+            }
+        }
+    
+}
